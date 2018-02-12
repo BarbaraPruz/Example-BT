db48d7e4df1b5a50ec0fc5975e9a2048a11a3b7f
barbara <barbara.prusiewicz@gmail.com>
2018-02-11 19:14:58 -1000

    Add files via upload
    Wonders of the world page

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 // navHTML just because no SSI or PHP
 const navHTML= '<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-black"> \
   <a class="navbar-brand" href="#"><img src="images\\earth.jpg"></a> \
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation"> \
+  <button class="navbar-toggler" id="toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation"> \
     <span class="navbar-toggler-icon"></span> \
   </button> \
   <div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault"> \
@@ -10,30 +10,39 @@ const navHTML= '<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-bla
     <a id="home" class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a> \
   </li> \
   <li class="nav-item dropdown"> \
-          <a class="nav-link dropdown-toggle" href="continents.html" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Continents</a> \
-          <div class="dropdown-menu" aria-labelledby="dropdown01"> \
-            <a class="dropdown-item" href="continents.html?africa">Africa</a> \
-            <a class="dropdown-item" href="continents.html?antartica">Antartica</a> \
-            <a class="dropdown-item" href="continents.html?asia">Asia</a> \
-            <a class="dropdown-item" href="continents.html?australia">Australia</a> \
-            <a class="dropdown-item" href="continents.html?europe">Europe</a> \
-            <a class="dropdown-item" href="continents.html?northAmerica">North America</a> \
-            <a class="dropdown-item" href="continents.html?southAmerica">South America</a>  \
-          </div> \
-      </li> \
-      <li class="nav-item dropdown"> \
+    <a class="nav-link dropdown-toggle" href="continents.html" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Continents</a> \
+    <div class="dropdown-menu" aria-labelledby="dropdown01"> \
+      <a class="dropdown-item" href="continents.html?africa">Africa</a> \
+      <a class="dropdown-item" href="continents.html?antartica">Antartica</a> \
+      <a class="dropdown-item" href="continents.html?asia">Asia</a> \
+      <a class="dropdown-item" href="continents.html?australia">Australia</a> \
+      <a class="dropdown-item" href="continents.html?europe">Europe</a> \
+      <a class="dropdown-item" href="continents.html?northAmerica">North America</a> \
+      <a class="dropdown-item" href="continents.html?southAmerica">South America</a>  \
+    </div> \
+</li> \
+<li class="nav-item dropdown"> \
           <a class="nav-link dropdown-toggle" href="oceans.html" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Oceans</a> \
           <div class="dropdown-menu" aria-labelledby="dropdown02"> \
-            <a class="dropdown-item" href="oceans.html#atlantic">Atlantic</a> \
-            <a class="dropdown-item" href="oceans.html#artic">Artic</a> \
-            <a class="dropdown-item" href="oceans.html#indian">Indian</a> \
-            <a class="dropdown-item" href="oceans.html#pacific">Pacific</a> \
-            <a class="dropdown-item" href="oceans.html#southern">Southern</a> \
+            <a class="dropdown-item" id="x" href="oceans.html#atlantic">Atlantic</a> \
+            <a class="dropdown-item" id="x" href="oceans.html#arctic">Arctic</a> \
+            <a class="dropdown-item" id="x" href="oceans.html#indian">Indian</a> \
+            <a class="dropdown-item" id="x" href="oceans.html#pacific">Pacific</a> \
+            <a class="dropdown-item" id="x" href="oceans.html#southern">Southern</a> \
           </div> \
       </li>  \
-      <li class="nav-item"> \
-        <a class="nav-link" href="#">Wonders</a> \
-      </li> \
+<li class="nav-item dropdown"> \
+  <a class="nav-link dropdown-toggle" href="wonders.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Wonders</a> \
+  <div class="dropdown-menu" aria-labelledby="dropdown03"> \
+    <a class="dropdown-item" href="wonders.html?aurora">Aurora</a> \
+    <a class="dropdown-item" href="wonders.html?grandCanyon">Grand Canyon</a> \
+    <a class="dropdown-item" href="wonders.html?greatBarrierReef">Great Barrier Reef</a> \
+    <a class="dropdown-item" href="wonders.html?harborRio">Harbor of Rio de Janiero</a> \
+    <a class="dropdown-item" href="wonders.html?mountEverest">Mount Everest</a> \
+    <a class="dropdown-item" href="wonders.html?paricutin">Par&iacute;cutin Volcano</a> \
+    <a class="dropdown-item" href="wonders.html?victoriaFalls">Victoria Falls</a>  \
+  </div> \
+</li> \
       <li class="nav-item"> \
         <a class="nav-link" href="about.html">About</a> \
       </li> \
@@ -51,7 +60,13 @@ $(document).ready(function() {
         var fn = url.slice(url.lastIndexOf("/")+1);
         console.log("nav js fn "+fn);
         $('#topNav').html(navHTML); // only because no SSI or PHP
-        $('ul.navbar-nav a[href="'+ fn +'"]').parent().addClass('active');        
-      
-
+        $('ul.navbar-nav a[href="'+ fn +'"]').parent().addClass('active');    
+        $(".dropdown-item").click(function(ev){ 
+          console.log ('click hamburger ' + $('#toggler').is(":visible"));
+          console.log (ev);
+          if ($('#toggler').is(":visible"))
+        //  $('.navbar-collapse').toggle();
+           $('.navbar-collapse').collapse('hide');
+        //   $('#navbarsExampleDefault').collapse();        
+        });
 });
---
+++ wonders.js
@@ -0,0 +1,32 @@
+var lastId='';
+const content = [
+    { name: 'aurora', pic: 'aurora.jpg', info: 'Aurora Borealis!'},
+    { name: 'grandCanyon', pic: 'grandcanyon.jpg',info: "Grand Canyon!"},
+    { name: 'greatBarrierReef', pic: 'greatBarrierReef.jpg', info: "Great Barrier Reef!"},
+    { name: 'harborRio', pic: 'harborRio.jpg',info: "Harbor of Rio de Janiero!"},
+    { name: 'mountEverest', pic: 'mountEverest.jpg',info: "Mount Everest!"},
+    { name: 'paricutin', pic: 'paricutin_volcano.jpg',info: "Par&iacute;cutin Volcano!"},
+    { name: 'victoriaFalls', pic: 'victoriaFalls.jpg',info: "Victoria Falls!"}          
+];
+
+const fillerText='Et oblique mnesarchum eam, dicta principes ea eos. Ullum constituto liberavisse pro eu, an usu alia aliquid. Aperiam labores laboramus ex mel. Eos affert fierent ne. Vim iudico tritani constituam ex, ad simul iudicabit mei. Per offendit salutandi cu, ut duo clita regione platonem. Has ne rebum delicatissimi, at vix libris ponderum. Vel cu mazim adolescens. Impetus sadipscing ea mei, id has integre explicari persecuti, est ex natum nihil fabellas. Vero erroribus posidonium nec ad. Suas nostro nusquam eu has, sed dicit omnes insolens in, te elit dicant eum. Probo inani maluisset eu his, homero semper oportere vim at. Id autem noster signiferumque eum.Et oblique mnesarchum eam, dicta principes ea eos. Ullum constituto liberavisse pro eu, an usu alia aliquid. Aperiam labores laboramus ex mel. Eos affert fierent ne. Vim iudico tritani constituam ex, ad simul iudicabit mei. Per offendit salutandi cu, ut duo clita regione platonem. Has ne rebum delicatissimi, at vix libris ponderum. Vel cu mazim adolescens. Impetus sadipscing ea mei, id has integre explicari persecuti, est ex natum nihil fabellas. Vero erroribus posidonium nec ad.';
+function getContent(ev) {
+    if (lastId) {
+        $('#'+lastId).css('color','black');
+    }
+    $('#'+ev.target.id).css('color','red');
+    lastId = ev.target.id;    
+    var idx = content.findIndex ( (v) => {return v.name===ev.target.id});  
+    $('#img').html('<img class="img-fluid" src="images\\'+content[idx].pic + '">'); 
+    $('#content').html('<h2>I am ' + content[idx].info + '</h2><p>'+fillerText+'</p>');  
+
+}
+
+
+$(document).ready(function() {
+    var href = window.location.href; 
+    var c = href.slice(href.lastIndexOf('?')+1);               
+    $('.navbtn').on('click', getContent);     
+    var e = jQuery.Event( 'click', { target: { id: c}} );
+    getContent(e);        
+});
